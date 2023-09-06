7705ae8e1138b61d739bc085d39a14bc929ee403
yvinod29 <vinuureddy555@gmail.com>
2023-09-06 10:34:15 +0530

    rest api 1

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/model/db');
+require('./app_api/model/db');
 var indexRouter = require('./app_server/routes/index');
+ 
+var routesApi = require('./app_api/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setupv
 app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'jade');
 
@@ -19,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', routesApi);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
